02f595e6c88ad92b7a4bae62bef2aea337a31d65
Matheus Phelipe <matheusphalves@gmail.com>
2021-10-06 12:54:13 -0300

    updated test. Extracting table's content

--- data-extraction.py
+++ data-extraction.py
@@ -2,6 +2,7 @@
 from tabula.io import read_pdf
 from zipfile import ZipFile
 import glob, os
+import csv
 
 
 class TableExtractor:
@@ -10,32 +11,34 @@ class TableExtractor:
         pass
 
     def extractTables(self, tableNames = ['Tabela de Tipo do Demandante', 'Tabela de Categoria do Padrão TISS', 'Tabela de Tipo de Solicitação'], pagesDelimiter = '108-114'):
-        #self.readPDFTables(tableNames=tableNames, pagesDelimiter=pagesDelimiter) #efetua a leitura e extração das tabelas do pdf, caso existam
-        #self.convertToCSV(tableNames)
+        self.readPDFTables(tableNames=tableNames, pagesDelimiter=pagesDelimiter) #efetua a leitura e extração das tabelas do pdf, caso existam
+        self.convertToCSV(tableNames)
         self.zipData('Teste_Intuitive_Care_{matheus_phelipe_alves_pinto}','csv')
 
-    def readPDFTables(self, tableNames, pagesDelimiter):
+    def readPDFTables(self, tableNames,  pagesDelimiter, columns = ['Código', 'Descrição da Categoria'],):
         self.tableList = read_pdf('padrao_tiss_componente_organizacional_202108.pdf', pages = pagesDelimiter)
         #passar link do pdf diretamente
         #filtrar tabelas
-        for item in self.tableList:
-            print(item)
-        pass
 
     def convertToCSV(self, tableNames):
+        tableNumber = -1
         if(len(self.tableList)):
-            for number in range(len(self.tableList)):
-                self.tableList[number].to_csv(tableNames[number]+ '.csv', sep='\t', na_rep = '   ', encoding='utf-8', index = False)
+            for table in self.tableList:
+                if('Tabela' in table.iloc[0].to_string()):
+                    tableNumber += 1
+                    table.to_csv(tableNames[tableNumber]+ '.csv', sep=';', encoding='utf-8', index = False)
+                else:
+                    table.to_csv(tableNames[tableNumber]+ '.csv', mode='a', sep=';', encoding='utf-8', index = False, header=False)
 
     def zipData(self, zipName, extensionType):
-        zipObject = ZipFile(f'{zipName}.zip', 'w')
         files = glob.glob(os.getcwd() + f"/*.{extensionType}") #obtem diretório atual
-        
-        for file in files:#exibe arquivos .csv a serem zipados
-            zipObject.write(file.split('\\')[-1])
-            print(file.split('\\')[-1])
-        zipObject.close()
-        print('[Created zip file at]: ', os.getcwd())
+        print(files)
+        if(len(files)>0):
+            zipObject = ZipFile(f'{zipName}.zip', 'w')
+            for file in files:#exibe arquivos .csv a serem zipados
+                zipObject.write(file.split('\\')[-1])
+            zipObject.close()
+            print(f'[Created zip file ({len(files)} files) at]: ', os.getcwd())
 
 
 tbExtractor = TableExtractor()

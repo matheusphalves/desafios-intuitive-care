8a15c16ae07880c690161924d72d9e922a67f1bd
Matheus Phelipe <matheusphalves@gmail.com>
2021-10-10 12:30:38 -0300

    update app

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,8 +5,8 @@
     </b-row>
     <b-row>
       <div style="margin:10px;">
-        <input v-model="searchText" style="margin-right:10px" type="text" placeholder="Digite alguma palavra chave">
-        <b-button @click="search(searchText)">Pesquisar</b-button>
+        <input v-model="inputValue" style="margin-right:10px" type="text" placeholder="Digite alguma palavra chave">
+        <b-button @click="search(inputValue)">Pesquisar</b-button>
         <p>
           Para visualizar a base de dados utilizada,
           <a href="http://www.ans.gov.br/externo/site_novo/informacoes_avaliacoes_oper/lista_cadop.asp" target="_blank" rel="noopener">clique aqui</a>.
@@ -14,31 +14,49 @@
       </div>
     </b-row>
     <b-row>
-      <Results></Results>
+    <div>
+        <div v-if="items">
+            <div v-if="items.length>0">
+            <h3>Resultados encontrados ({{items.length}})</h3>
+            <b-table striped hover :items="items"></b-table>
+            </div>
+        <div v-if="items.length==0">
+            <h3>Desculpe, não existem resultados para o termo buscado.</h3>
+        </div>
+        </div>      
+      </div>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import Results from './components/Results.vue'
 import axios from 'axios';
 
 export default {
   name: 'App',
+  props: [],
   components: {
-    Results
-  },     
+  },  
+  data() {
+      return {
+        inputValue: '',
+        fields: ['registroANS', 'cnpj', 'razaoSocial', 'nomeFantasia', 'modalidade',
+        'logradouro', 'numero', 'complemento', 'bairro', 'cidade', 'uf', 'cep', 'ddd', 'telefone', 'fax',
+        'enderecoEletronico', 'representante', 'cargoRepresentante', 'dataRegistroANS'],
+        items: null
+      }
+    },   
   methods:{
     search(text){
-      if(text){
+      if(text && text.length>=3){
         axios.post(`http://127.0.0.1:5100/query_items`, {query: text})
          .then((res) => {
-           console.log(res.data)
+           this.items = res.data
          })
          .catch((error) => {
+           this.items = []
            console.log(error);
          });
-        console.log(text)
       }   
     }
   }
